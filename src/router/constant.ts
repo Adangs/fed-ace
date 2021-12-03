31057bcac21e78e0fd79a94b7c16cc16f6a69370
Adang <lundang.liu@feima-inc.com>
2021-12-03 16:45:37 +0800

    vue2 to vue3

--- src/router/constant.ts
+++ src/router/constant.ts
@@ -1,17 +1,44 @@
+import LayoutOuter from '@/layout/index.vue'
+import { markRaw } from 'vue'
+
 export default [
   {
     path: '/',
-    name: 'Login',
-    component: () => import('@/views/login/index.vue'),
-    meta: {
-      hidden: false
-    }
-  },
-  {
-    path: '/404',
-    component: () => import('@/views/error/404.vue'),
-    meta: {
-      hidden: false
-    }
+    component: markRaw(LayoutOuter),
+    redirect: '/create',
+    children: [
+      {
+        path: 'create',
+        name: 'PageCreate',
+        component: () => import('@/views/page/create.vue'),
+        meta: {
+          title: '微页面创建'
+        }
+      },
+      {
+        path: 'edit',
+        name: 'PageEdit',
+        component: () => import('@/views/page/create.vue'),
+        meta: {
+          title: '微页面编辑'
+        }
+      },
+      {
+        path: 'copy',
+        name: 'PageCopy',
+        component: () => import('@/views/page/create.vue'),
+        meta: {
+          title: '微页面复制'
+        }
+      },
+      {
+        path: 'preview',
+        name: 'PagePreview',
+        component: () => import('@/views/page/preview.vue'),
+        meta: {
+          title: '微页面预览'
+        }
+      }
+    ]
   }
 ]
--- src/router/guards.ts
+++ src/router/guards.ts
@@ -1,48 +1,10 @@
-import { Router, RouteRecordRaw } from 'vue-router'
-import { local_storage } from '@/utils/storage'
-import $store from '@/store'
+import { Router } from 'vue-router'
 
-const whiteList = ['/', '/404']
 export function createRouterGuards(router: Router) {
   router.beforeEach(async (to, from, next) => {
     const Loading = window['$loading'] || null
     Loading && Loading.start()
-    if (local_storage.get('token')) {
-      if (to.path === '/login') {
-        next({ path: '#/' })
-      } else {
-        const { accountId } = $store.getters
-        if (accountId) {
-          // 判断当前用户是否已拉取完user_info信息
-          next()
-        } else {
-          try {
-            await $store.dispatch('user/getUserInfo')
-            await $store.dispatch('permission/generateRoutes')
-            const permissionRoutes: RouteRecordRaw[] = $store.state.permission.permissionRoutes
-            permissionRoutes.forEach((route) => {
-              router.addRoute(route)
-            })
-            next({
-              ...to,
-              replace: true
-            })
-          } catch (e) {
-            // 清除本地缓存用户数据并跳转到登录页重新登录
-            // $store.dispatch('user/logout')
-            next(`#/login?redirect=${to.fullPath}`)
-          }
-        }
-      }
-    } else {
-      if (whiteList.includes(to.path)) {
-        // 在免登录白名单中，直接进入
-        next()
-      } else {
-        // 其他没有访问权限的页面将重定向到登录页面。
-        next(`#/login?redirect=${to.fullPath}`)
-      }
-    }
+    next()
   })
 
   router.afterEach((to, from, failure) => {
